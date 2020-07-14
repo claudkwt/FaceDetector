7e71805f90dfa0836dea8ac45f8abe1dba64b961
Claud <54764931+claudkwt@users.noreply.github.com>
2020-07-14 15:28:27 +0800

    Update 02_face_training.py

--- 02_face_training.py
+++ 02_face_training.py
@@ -1,10 +1,14 @@
 import cv2
 import numpy as np
 from PIL import Image
-import os# Path for face image database
+import os
+
+# Path for face image database
 path = 'dataset'
 recognizer = cv2.face.LBPHFaceRecognizer_create()
-detector = cv2.CascadeClassifier("haarcascade_frontalface_default.xml");# function to get the images and label data
+detector = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+
+# function to get the images and label data
 def getImagesAndLabels(path):
     imagePaths = [os.path.join(path,f) for f in os.listdir(path)]     
     faceSamples=[]
@@ -17,7 +21,10 @@ def getImagesAndLabels(path):
         for (x,y,w,h) in faces:
             faceSamples.append(img_numpy[y:y+h,x:x+w])
             ids.append(id)
-    return faceSamples,idsprint ("\n [INFO] Training faces. It will take a few seconds. Wait ...")faces,ids = getImagesAndLabels(path)
+    return faceSamples,ids
+
+print("\n [INFO] Training faces. It will take a few seconds. Wait ...")
+faces,ids = getImagesAndLabels(path)
 recognizer.train(faces, np.array(ids))# Save the model into trainer/trainer.yml
 recognizer.write('trainer/trainer.yml') # Print the numer of faces trained and end program
 print("\n [INFO] {0} faces trained. Exiting Program".format(len(np.unique(ids))))
